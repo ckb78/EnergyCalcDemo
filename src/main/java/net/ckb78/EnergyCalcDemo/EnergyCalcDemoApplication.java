18d571c1ace880b1cc8c1eb003e53d72a2b7aa4e
Christian Bjørnsrud <ckb78@outlook.com>
2021-03-15 18:20:24 +0100

    Develop - First Commit

---
+++ src/main/java/net/ckb78/EnergyCalcDemo/EnergyCalcDemoApplication.java
@@ -0,0 +1,13 @@
+package net.ckb78.EnergyCalcDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergyCalcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnergyCalcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/ckb78/EnergyCalcDemo/EnergyCalcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.ckb78.EnergyCalcDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergyCalcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
