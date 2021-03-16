54a2d143dcfc7d2702a863c04a64822ab5a8fc56
Christian Bjørnsrud <ckb78@outlook.com>
2021-03-16 10:51:03 +0100

    Develop - Added simple logging to rest controller.

--- src/main/java/net/ckb78/EnergyCalcDemo/controller/ECRestController.java
+++ src/main/java/net/ckb78/EnergyCalcDemo/controller/ECRestController.java
@@ -16,29 +16,34 @@ public class ECRestController {
 
     @GetMapping("/getdata")
     public List<ECDto> getAllData() {
+        log.info("* GET /getdata");
         return calcService.getAllEnergyData();
     }
 
     @GetMapping("/getbyid/{id}")
     public ECDto getSingleEnergyData(@PathVariable Long id) {
+        log.info("* GET /getbyid/" + id);
         return calcService.getEnergyDataById(id);
     }
 
     @GetMapping("/getbycompany/{company}")
     public List<ECDto> getSingleEnergyData(@PathVariable String company) {
+        log.info("* GET /getbycompany/" + company);
         return calcService.getEnergyDataByCompany(company.toUpperCase());
-    }
-
-    @GetMapping("/populate")
-    public List<ECDto> populateWithTestData() {
-        return calcService.addTestData();
     }
 
     @PostMapping(path = "/inputdata", consumes = "application/json", produces = "application/json")
     public ECDto addData(@RequestBody DataInput input) {
+        log.info("* POST /inputdata " + "\n " + input.toString());
         if (calcService.validateInput(input)) {
             return calcService.createAndSaveResult(input);
         }
         return null;
+    }
+
+    @GetMapping("/populate")
+    public List<ECDto> populateWithTestData() {
+        log.info("* GET /populate ");
+        return calcService.addTestData();
     }
 }
